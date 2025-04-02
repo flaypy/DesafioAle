787682fa4634439056272785b02b0901aa6e46bc
flaypy <souoflay@gmail.com>
2025-04-02 17:16:58 -0300

    ai

--- src/ConsomeAPI.java
+++ src/ConsomeAPI.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class ConsomeAPI {
+    public static String buscarDados() throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://jsonplaceholder.typicode.com/posts"))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        return response.body();
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,71 @@
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Gson gson = new Gson();
+
+        String json = ConsomeAPI.buscarDados();
+        List<Post> posts = gson.fromJson(json, new TypeToken<List<Post>>(){}.getType());
+//        for (Post post : posts) {
+//            System.out.println(post);
+//        }
+
+        List<Post> filtragem = posts.stream()
+                .filter(post -> post.getTitle().contains("qui"))
+                .toList();
+//        for (Post post : filtragem) {
+//            System.out.println(post);
+//        }
+
+        List<Post> ordenacao = filtragem.stream()
+                .sorted(Comparator.comparingInt(Post::getUserId))
+                .toList();
+        System.out.println("Posts filtrados e ordenados: ");
+
+        List<Post> agrupamento = posts.stream()
+                .collect(Collectors.groupingBy(Post::getUserId))
+                .values()
+                .stream()
+                .flatMap(List::stream)
+                .toList();
+//        for (Post post : agrupamento) {
+//            System.out.println(post);
+//        }
+
+        int ids = filtragem.stream()
+                .map(Post::getId)
+                .reduce(0, Integer::sum);
+
+        Stream<String> mapeamento = ordenacao.stream()
+                .map(Post::getTitle)
+                .distinct();
+
+        for (Post post : ordenacao) {
+            System.out.println(post);
+        }
+
+        System.out.println("total de posts por UserId:");
+        Map<Integer, Long> totalPostsPorUserId = posts.stream()
+                .collect(Collectors.groupingBy(Post::getUserId, Collectors.counting()));
+        for (Map.Entry<Integer, Long> entry : totalPostsPorUserId.entrySet()) {
+            System.out.println("UserId: " + entry.getKey() + ", Total de posts: " + entry.getValue());
+        }
+
+        System.out.println("Total de ids: " + ids);
+
+        System.out.println("Titulos filtrados e ordenados: ");
+        for (String title : mapeamento.toList()) {
+            System.out.println("titulo:" + title);
+        }
     }
 }
